ec547df82bbd8f25a2c18aa6d64037aa3f0560b0
Thomas Seleiro <thomas.seleiro@postgrad.manchester.ac.uk>
2021-01-07 16:41:35 +0000

    Initial commit

---
+++ code/multiSign.m
@@ -0,0 +1,9 @@
+function [S, its] = multiSign(A)
+%MULTISIGN Computes matrix sign function in mixed single-double precision
+%   Computes the matrix sign function of input matrix A using a norm scaled
+%   Newton iteration in single precision. We then add a correction to the
+%   result to increase its commutativity with A in double precision, and
+%   perform a final double precision iteration.
+
+end
+
