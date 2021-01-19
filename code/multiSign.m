bcc2b1084df3aba27cf871c17910439c63c0563a
Thomas Seleiro <thomas.seleiro@postgrad.manchester.ac.uk>
2021-01-19 17:10:22 +0000

    Modifies multiSign to be more flexible like multiPoldec

--- code/multiSign.m
+++ code/multiSign.m
@@ -1,125 +1,142 @@
-function [singleS, S, its, devArray, commutArray, E] = multiSign(A, debug)
+function [S, its, E] ... 
+    = multiSign(A, type, debug)
 %multiSign Computes matrix sign function in mixed single-double precision.
 %   Computes the matrix sign function of input matrix A using a norm scaled
 %   Newton iteration in single precision. We then add a correction to the
 %   result to increase its commutativity with A in double precision, and
 %   perform a final double precision iteration.
 
-    %Check inputs
-    if(nargin == 1) debug = false; end
+    %%Process the input argument
+    switch nargin
+        case 1
+            debug = false;
+            type = "single";
+            switchPrecision = true;
+            typeRoundoff = 1e-8;
+        case 2
+            debug = false;
+            [type, switchPrecision, typeRoundoff] = processTypeInput(type);
+        case 3
+            debug = debug;
+            [type, switchPrecision, typeRoundoff] = processTypeInput(type);
+    end
     
-    n = size(A, 2);
-    k = 0;
-    muFunc = @(B, invB, p) sqrt(norm(invB, p) / norm(B, p));
-    
-    singleA = cast(A, "single");
-    singleX = cast(A, "single");
-    X = cast(A, "double");
-    singleXnew = zeros(n);
-    Xnew = zeros(n);
-    
-    %Array to store the single and double values for |X_{k+1} - X_k|/|X_k|
-    iterDist    = ones(1, 2);
-    %Array to store the single and double values for |X_k^2 - I| (ie the
-    %distance to an involutory matrix)
-    involDist   = ones(1, 2);
-    %List to store the deviation between single and double matrices
-    devArray    = [];
-    %List to store the commutativity of the iterates with A
-    commutArray = [];
+    %We declare variables to be used in the function
+    n = size(A, 1);
+    X = cast(A, type); Xnew = zeros(n);
+    iterDist = 1; involDist = norm(A*A - eye(n));
     
     %Print out debug headers if debugging
     if(debug)
         fprintf("\nk  \t |X_k-X_{k-1}|/|X_k| \t     |I - X_k^2|     \n");
         fprintf("---\t---------------------\t---------------------\n");
-        fprintf("   \t  Single     Double  \t  Single     Double  \n");
-        fprintf("===\t=====================\t=====================\n");
     end
     
-    %While loop that calculates the Newton iterates for the sign function
-    %in single and double precision, until the single precision iterate
-    %converges.
-    while(iterDist(1) >= 1e-8 * n && involDist(1) >= 1e-8 * n && k <= 100)
-        %Store the inverses X_k to only have to calculate them once.
-        singleInvX = inv(singleX);
-        invX = inv(X);
+    %Begin the loop to calculate the iterates
+    k = 0;
+    while(iterDist >= n*typeRoundoff && involDist >= n*typeRoundoff ...
+            && k <= 100)
+        %Calculate the next Newton iterate
+        [Xnew, iterDist, involDist] = signNewtonStep(X, k, debug);
         
-        %Calculate the scaling factors mu
-        singleMu = muFunc(singleX, singleInvX, inf);
-        mu = muFunc(X, invX, inf);
-        
-        %Calculate the new iterates
-        singleXnew = (singleMu * singleX + singleInvX / singleMu) / 2;
-        Xnew = (mu * X + invX / mu) / 2;
-        
-        %Calculate various metrics from the iterates
-        iterDist(1)  = norm(singleXnew - singleX, inf) / norm(singleXnew, inf);
-        iterDist(2)  = norm(Xnew - X, inf) / norm(Xnew, inf);
-        involDist(1) = norm(singleXnew*singleXnew - eye(n), inf);
-        involDist(2) = norm(Xnew*Xnew - eye(n), inf);
-        devArray     = [devArray norm(singleXnew - cast(Xnew, "single"), inf)];
-        commutArray  = [commutArray; norm(singleA*singleXnew - singleXnew * singleA, inf) norm(A*Xnew - Xnew*A, inf)];
+        %MORE METRICS FOR ANALYSIS CAN BE PLACED HERE
         
         %Move Xnew into X and increment k
-        singleX = singleXnew;
         X = Xnew;
         k = k+1;
-
-        %Print the metrics if debugging
-        if(debug)
-            fprintf("%3d\t%.4e %.4e\t%.4e %.4e\n", k, iterDist(1), iterDist(2), involDist(1), involDist(2));
+    end
+    its = k;
+    
+    %If specified in the function inputs, we switch precision and iterate
+    %again
+    if(switchPrecision && type == "single")
+        X = cast(X, "double");
+        typeRoundoff = 1e-16;
+    
+        %Print the distance from commutativity of the converted iterate
+        if (debug)
+            fprintf("\n----------CONVERTING TO DOUBLE PRECISION---------\n");
+            fprintf("Norm of the commutator:\t%e\n", norm(X*A - A*X, inf));
         end
-    end
-    
-    %We calculate the final iteration of the algorithm by converting the
-    %single precision iterate to double precision and running the algorithm
-    %one more time
-    castX = cast(singleX, "double");
-    
-    %Print the distance from commutativity of the converted iterate
-    if (debug)
-        fprintf("\n----------CONVERTING TO DOUBLE PRECISION---------\n");
-        fprintf("Norm of the commutator:\t%e\n", norm(castX*A - A*castX, inf));
-    end
-    
-    %We add a correction to the iterate to improve its commutativity with A
-    [castX, E] = commutLSQ(castX, A, debug);
-    %[castX, E] = randomCommut(castX, A, 10, debug);
-    
-    %We repeat one final iteration of the algorithm in double precision
-    castIts = 0;
-    castIterDist = 1;
-    castInvolDist = 1;
-    castXnew = zeros(n);
-    while(castIterDist >= 1e-16 * n && castInvolDist >= 1e-16 *n && k <= 100)
-        %Store the inverses X_k to only have to calculate them once.
-        castInvX = inv(castX);
 
-        %Calculate the scaling factors mu
-        castMu = muFunc(castX, castInvX, inf);
+        %We add a correction to the iterate to improve its commutativity with A
+        [X, E] = commutLSQ(X, A, debug);
 
-        %Calculate the new iterates
-        castXnew = (castMu * castX + castInvX / castMu) / 2;
-
-        %Calculate various metrics from the iterates
-        castIterDist  = norm(castXnew - castX, inf) / norm(castXnew, inf);
-        castInvolDist = norm(castXnew*castXnew - eye(n), inf);
-
-        %Move castXnew into castX and increment castIts
-        castX = castXnew;
-        castIts = castIts+1;
+        %We calculate at most 3 iterations, since quadratic convergence
+        %should ensure that the convergence is unitary after only one
+        %iteration (in practice this might not be guaranteed immediately
+        %and the accuracy can be improved by one or two more iterations)
+        newK = 0;
+        while(iterDist >= n*typeRoundoff && involDist >= n*typeRoundoff ...
+            && newK <= 3)
+            %Calculate the next Newton iterate
+            [Xnew, iterDist, involDist] = signNewtonStep(X,k+newK,debug);
+            
+            %MORE METRICS FOR ANALYSIS CAN BE PLACED HERE
+            if(debug)
+                fprintf("Norm of the commutator:\t%e\n", norm(X*A - A*X, inf));
+            end
         
-        %Print the metrics if debugging
-        if(debug)
-            fprintf("%3d\t%.4e           \t%.4e           \n", k+castIts, castIterDist, castInvolDist);
-            fprintf("Norm of the commutator:\t%e\n", norm(castX*A - A*castX, inf));
+            %Move Xnew into X and increment newK
+            X = Xnew;
+            newK = newK+1;
         end
+        its = its + newK;
     end
-    
-    singleS = castXnew;
+
+    %Form S to return it
     S = X;
-    its = k+castIts;
 end
+
+
+
+function [type, switchPrecision, typeRoundoff] = processTypeInput(inputType)
+%processTypeInput Processes the type argument for multiPoldec
+    if ismember(inputType, ["single", "s"])
+        type = "single";
+        switchPrecision = true;
+        typeRoundoff = 1e-8;
+    elseif ismember(inputType, ["double", "d"])
+        type = "double";
+        switchPrecision = false;
+        typeRoundoff = 1e-16;
+    elseif ismember(inputType, ["singleOnly", "so"])
+        type = "single";
+        switchPrecision = false;
+        typeRoundoff = 1e-8;
+    else
+        error('Input argument %s not recognised. Use "single", '...
+            + '"double" or "singleOnly"', inputType);
+    end
+end
+
+
+
+function [Xnew, iterDist, involDist] = signNewtonStep(X, k, debug)
+%poldecNewtonStep Computes one step of the polar decomp Newton iteration
+%   Function that computes the next iterate in the scaled Newton iteration
+%   using the inf norm scaling (to reduce the computational overhead from
+%   using the determinant and spectral scaling factor).
+%   Note that k corresponds to the index of the iterate X NOT Xnew
+
+    %Store the inverse to only calculate it once
+    invX = inv(X);
+    %Calculate the 1,inf norm scaling factor
+    mu = sqrt(norm(invX, inf) / norm(X, inf));
+    
+    %Calculate the new iterate
+    Xnew = (mu * X + invX / mu) / 2;
+    
+    %Calculate various metrics from the iterates
+    iterDist = norm(Xnew - X, inf) / norm(Xnew, inf);
+    involDist = norm(eye(n) - Xnew * Xnew, inf);
+    
+    %Print the metrics if debugging
+    if(debug)
+        fprintf("%3d\t  %.11e  \t  %.11e  \n", k+1, iterDist, involDist);
+    end
+end
+
 
 
 function [Xnew, E] = commutLSQ(X, A, debug)
