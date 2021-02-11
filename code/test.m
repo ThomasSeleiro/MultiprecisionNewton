b2d51c2b15f1d197fdb477ddb2f105805b8a1c65
Thomas Seleiro <thomas.seleiro@postgrad.manchester.ac.uk>
2021-02-11 00:13:38 +0000

    Adds parfor parallelism

--- code/test.m
+++ code/test.m
@@ -1,4 +1,4 @@
-NUM_TRIALS = 10; N = 100:100:1000; k = size(N,2);
+NUM_TRIALS = 4; N = 10:5:100; k = size(N,2);
 
 %Time to compute
 t_givens = zeros(1, k);
@@ -26,31 +26,56 @@ hermFactDiff = zeros(1, k);
 
 for i = 1:k
     n = N(i);
-    disp(n);
+    fprintf("n = %4d\n", n);
 
-    for j = 1:NUM_TRIALS
+    temp_t_givens = 0;
+    temp_t_newton = 0;
+    temp_s_givens = 0;
+    temp_unitDist_givens = 0;
+    temp_unitDist_newton = 0;
+    temp_hermDist_givens = 0;
+    temp_hermDist_newton = 0;
+    temp_accuracy_givens = 0;
+    temp_accuracy_newton = 0;
+    temp_unitFactDiff = 0;
+    temp_hermFactDiff = 0;
+
+    parfor j = 1:NUM_TRIALS
         A = rand(n);
 
         t = tic();
         [Ug, Hg, sweep] = maxtracePoldec(A);
-        t_givens(i) = t_givens(i) + toc(t)/NUM_TRIALS;
+        temp_t_givens = temp_t_givens + toc(t)/NUM_TRIALS;
 
         t = tic();
-        [Un, Hn, ~] = multiPoldec(A, "double", false);
-        t_newton(i) = t_newton(i) + toc(t)/NUM_TRIALS;
+        [Un, Hn, ~] = multiPoldec(A, "double", false, false);
+        temp_t_newton = temp_t_newton + toc(t)/NUM_TRIALS;
 
-        s_givens(i) = s_givens(i) + sweep/NUM_TRIALS;
+        temp_s_givens = temp_s_givens + sweep/NUM_TRIALS;
 
-        unitDist_givens(i) = unitDist_givens(i) + norm(eye(n) - Ug'*Ug, inf) / NUM_TRIALS;
-        unitDist_newton(i) = unitDist_newton(i) + norm(eye(n) - Un'*Un, inf) / NUM_TRIALS;
+        temp_unitDist_givens = temp_unitDist_givens + norm(eye(n) - Ug'*Ug, inf) / NUM_TRIALS;
+        temp_unitDist_newton = temp_unitDist_newton + norm(eye(n) - Un'*Un, inf) / NUM_TRIALS;
 
-        hermDist_givens(i) = hermDist_givens(i) + norm(Hg - Hg', inf) / (2 * NUM_TRIALS);
-        hermDist_newton(i) = hermDist_newton(i) + norm(Hn - Hn', inf) / (2 * NUM_TRIALS);
+        temp_hermDist_givens = temp_hermDist_givens + norm(Hg - Hg', inf) / (2 * NUM_TRIALS);
+        temp_hermDist_newton = temp_hermDist_newton + norm(Hn - Hn', inf) / (2 * NUM_TRIALS);
 
-        accuracy_givens(i) = accuracy_givens(i) + norm(A - Ug*Hg, inf) / NUM_TRIALS;
-        accuracy_newton(i) = accuracy_newton(i) + norm(A - Un*Hn, inf) / NUM_TRIALS;
+        temp_accuracy_givens = temp_accuracy_givens + norm(A - Ug*Hg, inf) / NUM_TRIALS;
+        temp_accuracy_newton = temp_accuracy_newton + norm(A - Un*Hn, inf) / NUM_TRIALS;
 
-        unitFactDiff(i) = unitFactDiff(i) + norm(Un - Ug, inf) / NUM_TRIALS;
-        hermFactDiff(i) = hermFactDiff(i) + norm(Hn - Hg, inf) / NUM_TRIALS;
+        temp_unitFactDiff = temp_unitFactDiff + norm(Un - Ug, inf) / NUM_TRIALS;
+        temp_hermFactDiff = temp_hermFactDiff + norm(Hn - Hg, inf) / NUM_TRIALS;
     end
+
+    t_givens(i) = temp_t_givens;
+    t_newton(i) = temp_t_newton;
+    s_givens(i) = temp_s_givens;
+    unitDist_givens(i) = temp_unitDist_givens;
+    unitDist_newton(i) = temp_unitDist_newton;
+    hermDist_givens(i) = temp_hermDist_givens;
+    hermDist_newton(i) = temp_hermDist_newton;
+    accuracy_givens(i) = temp_accuracy_givens;
+    accuracy_newton(i) = temp_accuracy_newton;
+    unitFactDiff(i) = temp_unitFactDiff;
+    hermFactDiff(i) = temp_hermFactDiff;
+
 end
