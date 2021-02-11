8d2332986b2f0fa208f5fcbf485fb9842df94a50
Thomas Seleiro <thomas.seleiro@postgrad.manchester.ac.uk>
2021-02-11 05:03:34 +0000

    MODIFIES TEST.M compares givens/poldec corrections

--- code/test.m
+++ code/test.m
@@ -1,27 +1,19 @@
-NUM_TRIALS = 4; N = 10:5:100; k = size(N,2);
+NUM_TRIALS = 8; N = 20:20:100; k = size(N,2);
 
 %Time to compute
 t_givens = zeros(1, k);
+t_poldec = zeros(1, k);
 t_newton = zeros(1, k);
 
 %Number of sweeps (givens only)
 s_givens = zeros(1, k);
-
-%Unitary polar factor distance
-unitDist_givens = zeros(1, k);
-unitDist_newton = zeros(1, k);
+s_poldec = zeros(1, k);
 
 %Symmetry of the Hermitian polar factor
 hermDist_givens = zeros(1, k);
+hermDist_poldec = zeros(1, k);
 hermDist_newton = zeros(1, k);
 
-%Accuracy of the polar decomposition
-accuracy_givens = zeros(1, k);
-accuracy_newton = zeros(1, k);
-
-%Difference between the polar factors
-unitFactDiff = zeros(1, k);
-hermFactDiff = zeros(1, k);
 
 
 for i = 1:k
@@ -29,53 +21,48 @@ for i = 1:k
     fprintf("n = %4d\n", n);
 
     temp_t_givens = 0;
+    temp_t_poldec = 0;
     temp_t_newton = 0;
     temp_s_givens = 0;
-    temp_unitDist_givens = 0;
-    temp_unitDist_newton = 0;
+    temp_s_poldec = 0;
     temp_hermDist_givens = 0;
+    temp_hermDist_poldec = 0;
     temp_hermDist_newton = 0;
-    temp_accuracy_givens = 0;
-    temp_accuracy_newton = 0;
-    temp_unitFactDiff = 0;
-    temp_hermFactDiff = 0;
 
     parfor j = 1:NUM_TRIALS
         A = rand(n);
 
+        [U1, H1, ~] = multiPoldec(A, "single", false, false);
+
         t = tic();
-        [Ug, Hg, sweep] = maxtracePoldec(A);
+        [~, Hg, sg, gFlag] = maxtracePoldec(H1, "double", false);
         temp_t_givens = temp_t_givens + toc(t)/NUM_TRIALS;
 
         t = tic();
-        [Un, Hn, ~] = multiPoldec(A, "double", false, false);
+        [~, Hp, sp, pFlag] = twobytwoPoldec(H1, "double", false);
+        temp_t_poldec = temp_t_poldec + toc(t)/NUM_TRIALS;
+
+        t = tic();
+        [~, Hn, ~] = multiPoldec(H1, "double", false, false);
         temp_t_newton = temp_t_newton + toc(t)/NUM_TRIALS;
 
-        temp_s_givens = temp_s_givens + sweep/NUM_TRIALS;
-
-        temp_unitDist_givens = temp_unitDist_givens + norm(eye(n) - Ug'*Ug, inf) / NUM_TRIALS;
-        temp_unitDist_newton = temp_unitDist_newton + norm(eye(n) - Un'*Un, inf) / NUM_TRIALS;
+        temp_s_givens = temp_s_givens + sg/NUM_TRIALS;
+        temp_s_poldec = temp_s_poldec + sp/NUM_TRIALS;
 
         temp_hermDist_givens = temp_hermDist_givens + norm(Hg - Hg', inf) / (2 * NUM_TRIALS);
+        temp_hermDist_poldec = temp_hermDist_poldec + norm(Hp - Hp', inf) / (2 * NUM_TRIALS);
         temp_hermDist_newton = temp_hermDist_newton + norm(Hn - Hn', inf) / (2 * NUM_TRIALS);
 
-        temp_accuracy_givens = temp_accuracy_givens + norm(A - Ug*Hg, inf) / NUM_TRIALS;
-        temp_accuracy_newton = temp_accuracy_newton + norm(A - Un*Hn, inf) / NUM_TRIALS;
-
-        temp_unitFactDiff = temp_unitFactDiff + norm(Un - Ug, inf) / NUM_TRIALS;
-        temp_hermFactDiff = temp_hermFactDiff + norm(Hn - Hg, inf) / NUM_TRIALS;
+        if(gFlag || pFlag) fprintf("Householder was used\n"); end
     end
 
     t_givens(i) = temp_t_givens;
+    t_poldec(i) = temp_t_poldec;
     t_newton(i) = temp_t_newton;
     s_givens(i) = temp_s_givens;
-    unitDist_givens(i) = temp_unitDist_givens;
-    unitDist_newton(i) = temp_unitDist_newton;
+    s_poldec(i) = temp_s_poldec;
     hermDist_givens(i) = temp_hermDist_givens;
+    hermDist_poldec(i) = temp_hermDist_poldec;
     hermDist_newton(i) = temp_hermDist_newton;
-    accuracy_givens(i) = temp_accuracy_givens;
-    accuracy_newton(i) = temp_accuracy_newton;
-    unitFactDiff(i) = temp_unitFactDiff;
-    hermFactDiff(i) = temp_hermFactDiff;
 
 end
